f0cf712ed0494145a62379c00c888afd99e75c2e
Chris Scott <christo247@gmail.com>
2020-03-01 14:30:20 -0800

    Add idempotent seed file for Resort data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,51 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Beginning seed of DB...'
+puts 'Seeding Resorts...'
+# Seed with a few resorts in diff areas
+SEED_RESORTS = [
+  {
+    uid:  'sierra',
+    name: 'Sierra At Tahoe',
+    lat:  38.8009,
+    lon:  -120.0809,
+  },
+  {
+    uid:  'kwood',
+    name: 'Kirkwood',
+    lat:  38.6848,
+    lon:  -120.0652,
+  },
+  {
+    uid:  'jhole',
+    name: 'Jackson Hole',
+    lat:  43.4799,
+    lon: -110.7624,
+  },
+  {
+    uid:  'alta',
+    name: 'Alta',
+    lat:  40.5884,
+    lon:  -111.6386,
+  },
+  {
+    uid:  'whistler',
+    name: 'Whistler Blackomb',
+    lat:  50.108333,
+    lon:  -122.9425,
+  }
+]
+
+
+# Find existing ones, create new ones that aren't found
+uids             = SEED_RESORTS.map { |resort| resort[:uid] }
+existing_resorts = Resort.where(uid: uids).pluck(:uid)
+
+puts "\t#{existing_resorts.size} resort(s) already exist..."
+
+new_resorts = SEED_RESORTS.reject do |resort|
+  existing_resorts.include?(resort[:uid])
+end
+
+puts "\tCreating #{new_resorts.size} new resort(s) in DB..."
+Resort.create!(new_resorts)
+
+puts 'Done.'
