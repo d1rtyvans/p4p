58b02b8a44ba08b186d02cd6e251fdc3be3140ab
Chris Scott <christo247@gmail.com>
2020-02-29 20:27:58 -0800

    Configure sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
+require 'sidekiq/web'
+
+Sidekiq::Web.set :session_secret, Rails.application.secrets[:secret_key_base]
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    # TODO: Read these and delete
+    # Protect against timing attacks:
+    # - See https://codahale.com/a-lesson-in-timing-attacks/
+    # - See https://thisdata.com/blog/timing-attacks-against-string-comparison/
+    # - Use digests to stop length information leaking (see also ActiveSupport::SecurityUtils.variable_size_secure_compare)
+    sidekiq_user = ENV.fetch('SIDEKIQ_USER')
+    sidekiq_pw   = ENV.fetch('SIDEKIQ_PW')
+    ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username),
+                                                ::Digest::SHA256.hexdigest(sidekiq_user)) &
+      ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password),
+                                                ::Digest::SHA256.hexdigest(sidekiq_pw))
+  end
+
+  mount Sidekiq::Web, at: '/asdf'
 end
